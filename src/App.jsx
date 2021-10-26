c4694304fc5a7597d23498dbc636ab44aaefd132
Monika Borowiak <m.borowiak91@gmail.com>
2021-10-26 21:03:41 +0100

    friends have name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Form } from "react-bootstrap";
 
 const client = new WebSocket("ws://127.0.0.1:8080");
 
@@ -8,86 +7,120 @@ function App() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [friends, setFriends] = useState([]);
-  const [recipient, setRecipient] = useState("");
+  const [recipientId, setRecipientId] = useState("");
+  const [name, setName] = useState("");
+  const [nameApprove, setNameApprove] = useState(false);
+  const [itisme, setItisme] = useState(true);
 
-  client.addEventListener("message", function (event) {
+  client.onmessage = function (event) {
     const msg = JSON.parse(event.data);
-    console.log(msg);
+    console.log(event);
     if (msg.type === "msg") {
       setMessages([...messages, msg.value]);
     }
     if (msg.type === "friends") {
       setFriends(msg.value);
     }
-  });
+    if (msg.type === "setName") {
+      setNameApprove(true);
+    }
+  };
 
   return (
     <>
       <div className="container-main">
-        <div className="row-1">
-          <div className="c-friends">
-            {friends.map((f, i) => (
-              <p
-                style={{
-                  ...(f === recipient ? { color: "pink" } : {}),
-                  cursor: "pointer",
-                }}
-                onClick={() => {
-                  setRecipient(f);
-                }}
-                key={i}
-              >
-                {f}
-              </p>
-            ))}
-          </div>
-          <div className="c-message">
-            <div className="message-container">
-              <div className="r-message-display">
-                {messages.map((m, i) => (
-                  <p key={i}>{m}</p>
+        {nameApprove ? (
+          <>
+            <div className="row-1">
+              <div className="c-friends">
+                {friends.map((f, i) => (
+                  <p
+                    style={{
+                      ...(f.id === recipientId ? { color: "pink" } : {}),
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      setRecipientId(f.id);
+                    }}
+                    key={i}
+                  >
+                    {f.name}
+                  </p>
                 ))}
               </div>
-              <div className="r-message-type">
-                <div className="c-form">
-                  <form>
-                    <textarea
-                      id="text"
-                      name="text"
-                      rows="3"
-                      cols="50"
-                      onChange={(e) => setMessage(e.target.value)}
-                      value={message}
-                    ></textarea>
-                  </form>
-                </div>
-                <div className="c-button">
-                  <button
-                    className="button"
-                    onClick={() => {
-                      if (recipient === "") {
-                        alert("choose recipient");
-                        return;
-                      }
+              <div className="c-message">
+                <div className="message-container">
+                  <div className="r-message-display">
+                    {messages.map((m, i) => (
+                      <p key={i}>{m}</p>
+                    ))}
+                  </div>
+                  <div className="r-message-type">
+                    <div className="c-form">
+                      <form>
+                        <textarea
+                          id="text"
+                          name="text"
+                          rows="3"
+                          cols="50"
+                          onChange={(e) => setMessage(e.target.value)}
+                          value={message}
+                        ></textarea>
+                      </form>
+                    </div>
+                    <div className="c-button">
+                      <button
+                        className="button"
+                        onClick={() => {
+                          if (recipientId === "") {
+                            alert("choose recipient");
+                            return;
+                          }
 
-                      client.send(
-                        JSON.stringify({
-                          value: message,
-                          type: "msg",
-                          to: recipient,
-                        })
-                      );
-                      setMessages([...messages, message]);
-                      setMessage("");
-                    }}
-                  >
-                    Send
-                  </button>
+                          console.log("sending");
+                          client.send(
+                            JSON.stringify({
+                              value: message,
+                              type: "msg",
+                              to: recipientId,
+                            })
+                          );
+                          setMessages([...messages, message]);
+                          setMessage("");
+                        }}
+                      >
+                        Send
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          <>
+            <div className="setName">
+              <h1>Name/Nickname</h1>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="submit"
+                value="Submit"
+                onClick={() => {
+                  client.send(
+                    JSON.stringify({
+                      value: name,
+                      type: "setName",
+                    })
+                  );
+                }}
+              />
+            </div>
+          </>
+        )}
       </div>
     </>
   );
